bb43daca9985b8ba778a331d7c683a864de35347
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-07 20:37:14 +0600

    Deleted tail of a doubly linked list

---
+++ deleteAtTail.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *previousPointer;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->previousPointer = NULL;
+        this->nextPointer = NULL;
+    }
+};
+
+void deleteAtTail(Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    Node *deleteTail = tail;
+    tail = tail->previousPointer;
+    tail->nextPointer = NULL;
+    delete deleteTail;
+}
+
+void printForward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << " ";
+        tmp = tmp->nextPointer;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *secondNode = new Node(20);
+    Node *tail = new Node(30);
+
+    head->nextPointer = secondNode;
+    secondNode->previousPointer = head;
+
+    secondNode->nextPointer = tail;
+    tail->previousPointer = secondNode;
+
+    deleteAtTail(head, tail);
+    printForward(head);
+
+    return 0;
+}
